ff36e5e95afa0eaeea4c61182ee35938c4dd5e96
Fahruraji <owatanabe82@gmail.com>
2021-11-14 10:24:12 +0800

    Initial commit

---
+++ app/src/main/java/com/fahruraji/a211androidn/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.fahruraji.a211androidn
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
